3b0bfc5b0be665eb12a261b16beca75c09a242ca
Gandharv Mahajan <gandharv.mahajan@gmail.com>
2025-02-16 15:37:10 -0800

    cleanup setup.py in driver package

--- src/driver/setup.py
+++ src/driver/setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
-from glob import glob
-import os
+
 
 package_name = 'driver'
 
@@ -12,8 +11,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'msg'), glob('msg/*.msg')),
-        (os.path.join('share', package_name, 'srv'), glob('srv/*.srv'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
